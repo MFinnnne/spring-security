593d6ace6e4438d66d1bf9255c05b90b4ea3c5eb
MFine <1737374338@qq.com>
2021-07-23 23:35:49 +0800

    init

---
+++ spring-security01/src/main/java/com/example/springsecurity01/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.springsecurity01.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author MFine
+ * @version 1.0
+ * @date 2021/7/23 22:50
+ **/
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .csrf().disable();
+    }
+}
